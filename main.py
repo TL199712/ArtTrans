8dc5eb53e50ae2636c5cd4d56f5eca719eaaa74c
TL199712 <66450063+TL199712@users.noreply.github.com>
2021-09-14 20:30:18 -0500

    Create main.py
    main file of the project

---
+++ main.py
@@ -0,0 +1,31 @@
+from Art import *
+import scipy.misc
+import matplotlib.pyplot as plt
+from IPython.display import Image
+for k in range (1,2):
+  for j in range (1,4):
+    content_path ='./pics/content_' + str (k) + '.jpg'
+    style_path = './pics/style_' + str (j) + '.jpg'
+    contentImage = scipy.misc.imread(content_path).astype(np.float)
+    styleImage = scipy.misc.imread(style_path).astype(np.float)
+    plt.imshow(contentImage/256)
+    plt.show()
+    plt.imshow(styleImage/256)
+    plt.show()
+    #contentImage = scipy.misc.imread("./examples/content_1.jpg").astype(np.float)
+    #styleImage = scipy.misc.imread("./examples/style_1.jpg").astype(np.float)
+    #contentLayerNames = ['relu4_2']
+    #styleLayerNames = ['relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1']
+    #######################################################
+    for i in range (1,5):
+      contentLayerNames = ['conv' + str(i) + '_1']
+      #contentLayerNames = ['conv1_1']
+      styleLayerNames = ['conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1'
+      resultImage = stylize(contentImage, styleImage,k,j,i, contentLayerNames = 
+      resultImage = resultImage[0,:,:,:]
+      img = np.clip(resultImage, 0, 255).astype(np.uint8)
+      output_path = './outputs/content' + str(k) + '_style' + str(j) + '_CLconv'
+      #scipy.misc.imsave("./outputs/content1_style1_CLconv11_SLconv11conv21conv31conv41conv51.jpg", img)
+      scipy.misc.imsave(output_path, img)
+      plt.imshow(img)
+      plt.show()
